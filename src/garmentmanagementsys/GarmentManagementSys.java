499c1fde3f18b80ab5682dd7e72ff7c18cd088bd
farihashammi <shammi23105101138@diu.edu.bd>
2024-11-17 13:04:06 +0600

    3rd commit

--- src/garmentmanagementsys/GarmentManagementSys.java
+++ src/garmentmanagementsys/GarmentManagementSys.java
@@ -1,9 +1,66 @@
 package garmentmanagementsys;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+class Garment {
+    public String id;
+    public String name;
+    public String description;
+    public String size;
+    public String color;
+    public double price;
+    public int stockQuantity; 
+
+    public Garment(String id, String name, String description, String size, String color, double price, int stockQuantity) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.size = size;
+        this.color = color;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public void updateStock(int quantity) {
+        stockQuantity += quantity;
+    }
+
+    public double calculateDiscountPrice(double discountPercentage) {
+        return price - (price * discountPercentage / 100);
+    }
+
+    public String getDetails() {
+        return "Garment ID: " + id + ", Name: " + name + ", Price: " + price + ", Stock: " + stockQuantity;
+    }
+}
+  
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 
 public class GarmentManagementSys {
 
 
     public static void main(String[] args) {
+        
+        
     }
     
 }
